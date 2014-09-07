cac8b620d3dbe55623774eb4ab8a5727f13f9456
unknown <christian@cellshift.com>
2014-09-07 13:52:44 -0700

    fixed navigation in Terrain scene. got carried away with spawning one block per Update

--- Assets/WuttyBehavior.js
+++
@@ -1,8 +0,0 @@
-﻿
-function Start () {
-	Debug.Log("hi there!", gameObject);
-}
-
-function Update () {
-
-}
--- Assets/trynaOVRPlayerController.js
+++ Assets/trynaOVRPlayerController.js
@@ -1,18 +1,41 @@
 ﻿#pragma strict
 
+// JavaScript
+function Start () {
+
+    for (var y = 0; y < 10; y++) {
+        for (var x = 0; x < 10; x++) {
+            var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.AddComponent(Rigidbody);
+            cube.transform.position = Vector3 (x*10, y*10, -5);
+        }
+    }
+}
 
 /// This script moves the character controller forward 
 /// and sideways based on the arrow keys.
 /// It also jumps when pressing space.
 /// Make sure to attach a character controller to the same game object.
 /// It is recommended that you make only one call to Move or SimpleMove per frame.	
-public var speed : float = 6.0;
+public var speed : float =  .5;
 public var jumpSpeed : float = 8.0;
 public var gravity : float = 20.0;
 private var moveDirection : Vector3 = Vector3.zero;
+//private var rotationDirection : Vector3 = Vector3.zero;
 
+public var step = 0;
 function Update() {
+
+	if (step%100 == 0)
+	{
+	        var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.AddComponent(Rigidbody);
+            cube.transform.position = Vector3 (step, step, -5);
+	}
+
+            
 	var controller : CharacterController = GetComponent(CharacterController);
+	
 	if (controller.isGrounded) {
 		// We are grounded, so recalculate
 		// move direction directly from axes
@@ -24,6 +47,9 @@ function Update() {
 		if (Input.GetButton ("Jump")) {
 			moveDirection.y = jumpSpeed;
 		}
+	} else
+	{
+	
 	}
 	// Apply gravity
 	moveDirection.y -= gravity * Time.deltaTime;
@@ -33,6 +59,3 @@ function Update() {
 }
 
 
-
-function Start () {
-}
