da0a4a3ccc25870eb5e5d6f84faf1ae6c5815d5a
j_shannon_p <johnshannonperkins@gmail.com>
2014-09-26 18:26:53 -0700

    saving before reverting to previous state-- issue is OVRController no gravity.

--- Assets/HandControllerController.cs
+++ Assets/HandControllerController.cs
@@ -1,9 +1,14 @@
-﻿using UnityEngine;
+﻿		using UnityEngine;
 using System.Collections;
 using Leap;
 
 public class HandControllerController : MonoBehaviour {
 	Controller controller;
+
+	public float speed = 0.000000001F;
+	public float gravity = 20.0F;
+	private Vector3 moveDirection = Vector3.zero;
+
 	
 	void Start ()
 	{
@@ -15,20 +20,43 @@ public class HandControllerController : MonoBehaviour {
 		Frame frame = controller.Frame();
 		if (frame.Hands.Count > 0) {
 			Hand frontHand = frame.Hands.Frontmost;
+			float handX = frontHand.PalmPosition.x;
+			float handY = frontHand.PalmPosition.y;
 
-			Debug.Log("HandPos Y: " + frontHand.PalmPosition.y);
-			Debug.Log("HandPos X: " + frontHand.PalmPosition.x);
+			//handX near:70/0, far:250/1
+			//handY left:200, right:-200 
+
+			float moveX = 0;
+			float moveZ = 0;
+
+			if(handX>120){
+				moveZ = 1;
+			} else if(handX<90){
+				moveZ = -1;
+			}
+
+			if(handY<-30){
+				moveX = 1;
+			} else if(handY>30){
+				moveX = -1;
+			}
 
 
+			CharacterController characterController = GetComponent<CharacterController>();
+			Debug.Log("characterController.isGrounded: " + characterController.isGrounded);
+			if (characterController.isGrounded) {
+				moveDirection = new Vector3(moveX, 0, moveZ);
+				moveDirection = transform.TransformDirection(moveDirection);
+				moveDirection *= speed;
 
-			//PointableList pointables = frontHand.Pointables;
-			/*FingerList indexFingerList = frontHand.Fingers.FingerType(Finger.FingerType.TYPE_INDEX);
-			Finger indexFinger = indexFingerList[0];
-			if(indexFinger.IsValid && indexFinger.IsExtended){
-				Debug.Log("Index Finger: " + indexFinger);
-				Debug.Log("indexFinger_X: "+indexFinger.TipPosition.x);
-				Debug.Log("indexFinger_Y: "+indexFinger.TipPosition.y);
-			}*/
+				Debug.Log("moveZ: " + moveZ);
+				Debug.Log("moveDirection: " + moveDirection);
+				
+			}
+			//moveDirection.y -= gravity * Time.deltaTime;
+			characterController.Move(moveDirection * Time.deltaTime);
+
+
 		}
 		Debug.Log("vvv   vvv   vvv   vvv   vvv   vvv   vvv   vvv");
 
---
+++ Assets/LeapHandEventListener.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+// Attach this script to a HandModel object.
+public class LeapHandEventListener : MonoBehaviour {
+	public CharacterController OVRController;
+
+	public float speed = 0.000000001F;
+	public float gravity = 20.0F;
+	private Vector3 moveDirection = Vector3.zero;
+
+	void Update() {
+		HandModel hand_model = GetComponent<HandModel>();
+		CharacterController characterController = OVRController.GetComponent<CharacterController> ();
+
+		// Examples of getting data.
+		Vector3 palm_position = hand_model.GetPalmPosition();
+		Quaternion palm_rotation = hand_model.GetPalmRotation();
+		Vector3 index_tip = hand_model.fingers[1].GetTipPosition();
+
+		Debug.Log ("characterController: "+ characterController);
+		Debug.Log ("palm_position.xex : " + palm_position.x);
+		Debug.Log ("palm_position.y: " + palm_position.y);
+		Debug.Log ("palm_position.z: " + palm_position.z);
+
+		float moveX = 0;
+		float moveZ = 0;
+		
+		/*if(handX>120){
+			moveZ = 1;
+		} else if(handX<90){
+			moveZ = -1;
+		}
+		
+		if(handY<-30){
+			moveX = 1;
+		} else if(handY>30){
+			moveX = -1;
+		}*/
+
+		Debug.Log ("characterController.isGrounded: "+ characterController.isGrounded);
+		if (characterController.isGrounded) {
+			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			Debug.Log("moveDirection: " + moveDirection);
+			moveDirection = transform.TransformDirection(moveDirection);
+			moveDirection *= speed;
+
+			Debug.Log("moveDirection: " + moveDirection);
+			
+		}
+		//moveDirection.y -= gravity * Time.deltaTime;
+		Debug.Log("Time.deltaTime: " + Time.deltaTime);
+		Debug.Log("moveDirection * Time.deltaTime: " + moveDirection * Time.deltaTime);
+		characterController.Move(moveDirection * Time.deltaTime);	
+
+	}
+}
