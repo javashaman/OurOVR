76f045c0790d6f61c3fc816e979d0385d21d5591
j_shannon_p <johnshannonperkins@gmail.com>
2014-09-27 17:04:28 -0700

    saving again. re-attached leapeventlistener script and starting to again figuring out how to move with hand.

--- Assets/LeapHandEventListener.cs
+++ Assets/LeapHandEventListener.cs
@@ -4,28 +4,28 @@ using Leap;
 
 // Attach this script to a HandModel object.
 public class LeapHandEventListener : MonoBehaviour {
-	public OVRCameraController camera;
+	public Transform ovr;
 
 	public float speed = 2.0F;
 	public float gravity = 20.0F;
-	//private Vector3 moveDirection = Vector3.zero;
+	private Vector3 moveDirection = Vector3.zero;
 
 	void Update() {
 		HandModel hand_model = GetComponent<HandModel>();
-		//CharacterController characterController = camera.GetComponent<CharacterController> ();
+		CharacterController characterController = ovr.GetComponent<CharacterController> ();
 
 		// Examples of getting data.
 		Vector3 palm_position = hand_model.GetPalmPosition();
-		Quaternion palm_rotation = hand_model.GetPalmRotation();
-		Vector3 index_tip = hand_model.fingers[1].GetTipPosition();
+		//Quaternion palm_rotation = hand_model.GetPalmRotation();
+		//Vector3 index_tip = hand_model.fingers[1].GetTipPosition();
 
-		//Debug.Log ("characterController: "+ characterController);
+		Debug.Log ("characterController: "+ characterController);
 		Debug.Log ("palm_position.xex : " + palm_position.x);
 		Debug.Log ("palm_position.y: " + palm_position.y);
 		Debug.Log ("palm_position.z: " + palm_position.z);
 
-		float moveX = 0;
-		float moveZ = 0;
+		//float moveX = 0;
+		//float moveZ = 0;
 		
 		/*if(handX>120){
 			moveZ = 1;
@@ -39,8 +39,8 @@ public class LeapHandEventListener : MonoBehaviour {
 			moveX = -1;
 		}*/
 
-		//Debug.Log ("characterController.isGrounded: "+ characterController.isGrounded);
-		/*if (characterController.isGrounded) {
+		Debug.Log ("characterController.isGrounded: "+ characterController.isGrounded);
+		if (characterController.isGrounded) {
 			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 			Debug.Log("moveDirection: " + moveDirection);
 			moveDirection = transform.TransformDirection(moveDirection);
@@ -48,11 +48,10 @@ public class LeapHandEventListener : MonoBehaviour {
 
 			Debug.Log("moveDirection: " + moveDirection);
 			
-		}*/
+		}
 		//moveDirection.y -= gravity * Time.deltaTime;
 		Debug.Log("Time.deltaTime: " + Time.deltaTime);
-		//Debug.Log("moveDirection * Time.deltaTime: " + moveDirection * Time.deltaTime);
-		Debug.Log ("NO MOVE MORE NO!");
+		Debug.Log("moveDirection * Time.deltaTime: " + moveDirection * Time.deltaTime);
 		//characterController.Move(moveDirection * Time.deltaTime);	
 
 	}
